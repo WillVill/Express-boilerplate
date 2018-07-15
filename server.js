b37bcccdc39f19fb5fadc6530756be73e6312f58
William Bech <bech.william2@gmail.com>
2018-07-15 13:07:32 +0200

    connected wooCommerce + products endpoint

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const express = require('express');
+import 'babel-core/register';
+import 'babel-polyfill';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+import { PORT } from './constants';
+import routes from './routes';
+
 const app = express();
-const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
-var router = express.Router();              // get an instance of the express Router
+let port = process.env.PORT || PORT;
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+// Error handling
+app.use((err, request, response, next) => {
+  console.log('Error', err);
+  response.status(500).send('Something broke!')
+})
 
-app.use('/api', router);
+app.use('/', routes);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
